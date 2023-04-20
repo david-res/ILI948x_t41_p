1f17b3199a25c4bc6a16a594568c5b42fc493f66
david-res <52652879+david-res@users.noreply.github.com>
2023-04-20 23:23:39 +0300

    Update ILI948x_t41_p_test.ino

--- examples/ILI948x_t41_p_test/ILI948x_t41_p_test.ino
+++ examples/ILI948x_t41_p_test/ILI948x_t41_p_test.ino
@@ -1,18 +1,28 @@
 #include "ILI948x_t41_p.h"
 #include "flexio_teensy_mm.c"
-ILI948x_t41_p lcd = ILI948x_t4_mm(13,11,12); //(dc, cs, rst)
+ILI948x_t41_p lcd = ILI948x_t41_p(10,8,9); //(dc, cs, rst)
+
+const uint16_t screenWidth = 480;
+const uint16_t screenHeight = 320;
+
+uint16_t dispBuffer[screenWidth * screenHeight];
+
 
 void setup() {
   Serial.begin(115200);
   delay(1000);
   Serial.print(CrashReport);
+  pinMode(4, OUTPUT);
+  digitalWrite(4, HIGH);
+
+  //memset(dispBuffer, 0x07e0,sizeof(dispBuffer));
   
-  lcd.begin();
+  lcd.begin(24);
   lcd.setRotation(3);
 }
 
 void loop() {
-  delay(1000);
+
   lcd.pushPixels16bit(flexio_teensy_mm,0,0,479,319); // 480x320
   delay(1000);
   lcd.pushPixels16bitAsync(flexio_teensy_mm,0,0,479,319); // 480x320
